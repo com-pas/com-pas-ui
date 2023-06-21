136495874a7720a16888095b597d7b70beffb615
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-06-21 22:02:35 +0200

    Added core/dist to gitignore

--- packages/core/dist/foundation/editing.d.ts
+++
@@ -1,7 +0,0 @@
-import { Edit } from './edit-event.js';
-export declare function localAttributeName(attribute: string): string;
-export declare function handleEdit(edit: Edit): Edit;
-export type LogEntry = {
-    undo: Edit;
-    redo: Edit;
-};
