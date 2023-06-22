06f99cc8930f9193e271a001e245eb345c498d64
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-06-22 12:27:57 +0200

    Remove rollup cache

--- plugins/oscd-substation/.rollup.cache/Users/pascalwilbrink/Documents/Workspaces/Alliander/lerna/open-scd/plugins/oscd-substation/dist/oscd-substation-editor.d.ts
+++
@@ -1,6 +0,0 @@
-import { LitElement } from 'lit';
-export default class OscdSubstationEditor extends LitElement {
-    foo(): void;
-    protected renderFoot(): import("lit-html").TemplateResult<1>;
-    render(): import("lit-html").TemplateResult<1>;
-}
